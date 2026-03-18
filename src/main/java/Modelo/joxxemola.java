07b9a4d60b82c5553ced9e0152f244c3b43fa331
Jose D Molano P <jdmolanoperdomo@gmail.com>
2026-03-18 09:22:33 -0500

    Commit 1

---
+++ src/main/java/Modelo/joxxemola.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class joxxemola {
+
+    System.out.println("Hello World!, i´m joxxemola");
+}
