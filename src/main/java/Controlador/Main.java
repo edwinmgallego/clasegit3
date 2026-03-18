41525983313104e145fc28c9485dc3ad043e8823
BeingJuan <juanarroyaver17@gmail.com>
2026-03-18 11:01:43 -0500

    Parece complejo pero es lo lindo de la programación

---
+++ src/main/java/Controlador/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Controlador;
+import Modelo.Yo;
+/**
+ *
+ * @author JuanA
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Yo persona = new Yo();
+
+        persona.nombre = "Juan";
+        persona.edad = 20;
+        persona.presentacion();
+    }
+}
+    
+
---
+++ src/main/java/Modelo/Yo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author JuanA
+ */
+public class Yo {
+    public String nombre;
+    public int edad;
+
+    public void presentacion() {
+        System.out.println("Hola, soy " + nombre + " y tengo " + edad + " años");
+    }
+}
