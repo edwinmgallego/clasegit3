6443e06a7669457679976bde48a4dfb78e8266fd
emgallego <emgallego@D125074.uao.edu.co>
2026-03-18 08:42:39 -0500

    este es mi primer push

---
+++ src/main/java/com/mycompany/clasegitflow/ClaseGitFlow.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clasegitflow;
+
+/**
+ *
+ * @author emgallego
+ */
+public class ClaseGitFlow {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
