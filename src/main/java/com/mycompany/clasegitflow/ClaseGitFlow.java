9f0104347ca1edcd6b366576197ee54577c2e8e4
USUARIO <Yeidy Caicedo>
2026-03-18 10:47:00 -0500

    Este es el commit que muestra mi nombre

--- src/main/java/com/mycompany/clasegitflow/ClaseGitFlow.java
+++ src/main/java/com/mycompany/clasegitflow/ClaseGitFlow.java
@@ -1,16 +1,19 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.clasegitflow;
 
 /**
  *
  * @author emgallego
  */
+import Modelo.yeicaicedo;
+
 public class ClaseGitFlow {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        yeicaicedo modelo = new yeicaicedo();
+        String nombre = modelo.mostrarNombre();
+        System.out.println(nombre);
     }
 }
