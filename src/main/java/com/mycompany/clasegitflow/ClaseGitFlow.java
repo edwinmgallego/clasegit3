4993699e113aa93258e55d0f977270fe65996202
felip <felip@11.11.33.38>
2026-03-18 11:10:37 -0500

    Hacer commit y aprender poo es increible, aqui esta mi nombre

--- src/main/java/com/mycompany/clasegitflow/ClaseGitFlow.java
+++ src/main/java/com/mycompany/clasegitflow/ClaseGitFlow.java
@@ -8,10 +8,14 @@ package com.mycompany.clasegitflow;
  *
  * @author emgallego
  */
+import Modelo.felipegarzon;
+
 public class ClaseGitFlow {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
+        System.out.println("TuNombreAqui: ");
+        felipegarzon modelo = new felipegarzon();
+        String nombre = modelo.mostrarNombre();
+        System.out.println(nombre);
     }
 }
