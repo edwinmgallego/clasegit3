d524134b5756f7724722b92d1c6d9fd6b3382e78
felip <felip@11.11.33.38>
2026-03-18 19:02:01 -0500

    Agrego clase felipegarzon y su uso en main

--- src/main/java/com/mycompany/clasegitflow/ClaseGitFlow.java
+++ src/main/java/com/mycompany/clasegitflow/ClaseGitFlow.java
@@ -1,21 +1,41 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.clasegitflow;
 
 /**
  *
- * @author emgallego
+ * @author felip
  */
+import Modelo.samuelbd8;
+import Modelo.samuelmurillo03;
+import Modelo.isaquijano;
 import Modelo.felipegarzon;
 
 public class ClaseGitFlow {
 
     public static void main(String[] args) {
+
+
+   
+        samuelbd8 consultor = new samuelbd8 ("Samuel Beltran");
+        
+        System.out.println("El nombre es: " + consultor.getNombre());
         System.out.println("TuNombreAqui: ");
-        felipegarzon modelo = new felipegarzon();
+        isaquijano modelo = new isaquijano();
         String nombre = modelo.mostrarNombre();
         System.out.println(nombre);
+         samuelmurillo03 nom = new samuelmurillo03();
+        nom.mostrarNombre();
+         System.out.println("TuNombreAqui: ");
+        felipegarzon modeloFelipe = new felipegarzon();
+        String nombreFelipe = modeloFelipe.mostrarNombre();
+        System.out.println(nombreFelipe);
     }
 }
+
+    
+
+
+    
